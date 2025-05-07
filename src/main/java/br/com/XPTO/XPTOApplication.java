7914326cce28d818a902a47a71cc2a613f4a1727
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-05-07 15:06:03 -0300

    build: config gitidnore, docker-compose and pom

---
+++ src/main/java/br/com/XPTO/XPTOApplication.java
@@ -0,0 +1,14 @@
+package br.com.XPTO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+XPTOApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XPTOApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/XPTO/XPTOApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.XPTO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XPTOApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
