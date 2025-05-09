f6f3e0692052b50a156a34b4266a5e9453c33be2
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-05-09 09:40:33 -0300

    feat: add movementCredit method POST from AccountController

--- src/main/java/br/com/XPTO/infra/presentation/AccountController.java
+++ src/main/java/br/com/XPTO/infra/presentation/AccountController.java
@@ -1,14 +1,18 @@
 package br.com.XPTO.infra.presentation;
 
 import br.com.XPTO.core.usecases.account.CreateBankAccountUsecase;
+import br.com.XPTO.core.usecases.account.MakeBankMovementUsecase;
 import br.com.XPTO.infra.dtos.account.responses.AccountResponse;
+import br.com.XPTO.infra.dtos.bankMovement.requests.BankMovementRequest;
+import br.com.XPTO.infra.dtos.bankMovement.responses.BankMovementResponse;
 import br.com.XPTO.infra.mappers.account.AccountDtoMapper;
+import br.com.XPTO.infra.mappers.bankMovements.BankMovementDtoMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/v1/account")
@@ -16,15 +20,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountController {
 
     private final CreateBankAccountUsecase createBankAccountUsecase;
+    private final MakeBankMovementUsecase makeBankMovementUsecase;
 
-    private final AccountDtoMapper mapper;
+    private final AccountDtoMapper accountDtoMapper;
+    private final BankMovementDtoMapper bankMovementDtoMapper;
 
     @PostMapping("/create")
     public ResponseEntity<AccountResponse> createBankAccount(){
 
         return ResponseEntity.status(HttpStatus.CREATED).body(
-                mapper.toDto(
+                accountDtoMapper.toDto(
                         createBankAccountUsecase.execute()
+                )
+        );
+    }
+
+    @PostMapping("/movement/{id}")
+    public ResponseEntity<BankMovementResponse> movementCredit(@PathVariable Long id, @Valid @RequestBody BankMovementRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                bankMovementDtoMapper.toDto(
+                        makeBankMovementUsecase.execute(id, bankMovementDtoMapper.toDomain(request)
+                        )
                 )
         );
     }
