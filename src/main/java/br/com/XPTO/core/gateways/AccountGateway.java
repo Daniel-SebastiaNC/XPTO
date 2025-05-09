41e2a76d02ba894275199d1f690a6a2afd46143f
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-05-09 09:36:19 -0300

    feat: add method findAccountById from AccountGateway

--- src/main/java/br/com/XPTO/core/gateways/AccountGateway.java
+++ src/main/java/br/com/XPTO/core/gateways/AccountGateway.java
@@ -1,7 +1,11 @@
 package br.com.XPTO.core.gateways;
 
 import br.com.XPTO.core.domains.Account;
+import br.com.XPTO.core.domains.BankMovement;
+
+import java.util.Optional;
 
 public interface AccountGateway {
     Account saveAccount(Account account);
+    Optional<Account> findAccountById(Long id);
 }
--- src/main/java/br/com/XPTO/infra/gateways/AccountRepositoryGateway.java
+++ src/main/java/br/com/XPTO/infra/gateways/AccountRepositoryGateway.java
@@ -4,14 +4,18 @@ import br.com.XPTO.core.domains.Account;
 import br.com.XPTO.core.gateways.AccountGateway;
 import br.com.XPTO.infra.mappers.account.AccountEntityMapper;
 import br.com.XPTO.infra.persistence.repositories.AccountRepository;
+import br.com.XPTO.infra.persistence.repositories.BankMovementRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class AccountRepositoryGateway implements AccountGateway {
-
     private final AccountRepository repository;
+    private final BankMovementRepository bankMovementRepository;
+
     private final AccountEntityMapper mapper;
 
     @Override
@@ -21,5 +25,10 @@ public class AccountRepositoryGateway implements AccountGateway {
                         mapper.toEntity(account)
                 )
         );
+    }
+
+    @Override
+    public Optional<Account> findAccountById(Long id) {
+        return repository.findById(id).map(mapper::toDomain);
     }
 }
