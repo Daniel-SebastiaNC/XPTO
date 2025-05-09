f8ebe0a99c6ebe07132b9364d85fe1a9ecedc5b5
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-05-09 09:38:24 -0300

    feat(bean): add bean to MakeBankMovementUsecase

--- src/main/java/br/com/XPTO/infra/beans/BeanConfiguration.java
+++ src/main/java/br/com/XPTO/infra/beans/BeanConfiguration.java
@@ -1,8 +1,11 @@
 package br.com.XPTO.infra.beans;
 
 import br.com.XPTO.core.gateways.AccountGateway;
+import br.com.XPTO.core.gateways.BankMovementGateway;
 import br.com.XPTO.core.usecases.account.CreateBankAccountUsecase;
 import br.com.XPTO.core.usecases.account.CreateBankAccountUsecaseImpl;
+import br.com.XPTO.core.usecases.account.MakeBankMovementUsecase;
+import br.com.XPTO.core.usecases.account.MakeBankMovementUsecaseImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,5 +14,10 @@ public class BeanConfiguration {
     @Bean
     public CreateBankAccountUsecase createBankAccountUsecase(AccountGateway accountGateway) {
         return new CreateBankAccountUsecaseImpl(accountGateway);
+    }
+
+    @Bean
+    public MakeBankMovementUsecase makeBankMovementUsecase(AccountGateway accountGateway, BankMovementGateway bankMovementGateway) {
+        return new MakeBankMovementUsecaseImpl(accountGateway, bankMovementGateway);
     }
 }
