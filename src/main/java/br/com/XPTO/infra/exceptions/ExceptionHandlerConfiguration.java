2ce68f735d07af616f935014e09205bccdda4fdf
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-05-09 09:29:19 -0300

    feat: update ExceptionHandler to MethodArgumentNotValidException and NotEnoughMoneyAccountException

--- src/main/java/br/com/XPTO/infra/exceptions/ExceptionHandlerConfiguration.java
+++ src/main/java/br/com/XPTO/infra/exceptions/ExceptionHandlerConfiguration.java
@@ -1,6 +1,8 @@
 package br.com.XPTO.infra.exceptions;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -18,5 +20,25 @@ public class ExceptionHandlerConfiguration {
         error.put("error: ", ex.getMessage());
 
         return error;
+    }
+
+    @ExceptionHandler(NotEnoughMoneyAccountException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public Map<String, String> handlerNotEnoughMoneyAccountException(NotEnoughMoneyAccountException ex){
+        HashMap<String, String> error = new HashMap<>();
+        error.put("error: ", ex.getMessage());
+
+        return error;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            errors.put(((FieldError) error).getField(), error.getDefaultMessage());
+        });
+
+        return errors;
     }
 }
